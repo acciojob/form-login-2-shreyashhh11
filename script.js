11202591f30507ac57cf66d16b2b4d8d643a393f
shreyash <154959751+shreyashhh11@users.noreply.github.com>
2026-03-29 14:48:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function(){
+	let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let phone = document.getElementById("Phone").value;
+    let email = document.getElementById("email").value;
+
+    let message = "First Name: " + fname + " " +
+                  "Last Name: " + lname + " " +
+                  "Phone Number: " + phone + " " +
+                  "Email ID: " + email;
+
+    alert(message);
+})
